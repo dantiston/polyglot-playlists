346fc4d9cb99c5969ea7d6961205af8583340af8
T.J. Trimble <trimblet@me.com>
2023-09-10 12:48:30 -0700

    initial setup; initial data

---
+++ polyglot-playlists/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = (): React$Element<"div"> => (
+  <div className="App">
+    <header className="App-header">
+      <p>Hello World!</p>
+    </header>
+  </div>
+);
+
+export default App;
